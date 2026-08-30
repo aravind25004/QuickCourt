eb9b762bc1865015b88b100b8e6b5c3ca42d7272
Sadhana-Jagan <sadhanajagan2004@gmail.com>
2026-08-30 12:40:06 +0530

    users route and controller udpated

--- backend/src/controller/adminController.js
+++ backend/src/controller/adminController.js
@@ -1,3 +1,42 @@
+import mongoose from "mongoose";
+import Facilities from "../models/Facilities";
+
 export async function getAllFacilities(req,res) {
-    
+    const facilities = await Facilities.find();
+    return res.status(200).json(facilities);
 }
+
+export async function approveFacility(req,res) {
+    const facilityid = req.params.facilityid;
+    const approve = req.params.approval;
+    const comments = req.params.comments || "";
+    const facility = await Facilities.findOne(facilityid);
+    if(!facility){
+        return res.status(404).json({message:"facility not found"})
+    }
+    if(approve.toLowerCase()!=="approved" && approve.toLowerCase()!=="rejected"){
+        return res.status(400).json({message:"Provide valid approval"});
+    }
+    const approvalStatus = facility.approvalStatus;
+    if(approvalStatus!=="PENDING"){
+        res.status(400).json({message:"cannot approve or reject"});
+    }
+    facility.approvalStatus = approve.toUpperCase();
+    facility.rejectionReason = comments;
+    facility.approvedBy = req.userid;
+    facility.approvedAt = new Date(); 
+    
+    await facility.save();
+    return res.status(200).json({message:"Status Updated",facility});
+}
+
+export async function getFacility(req,res) {
+    const facilityid = req.params.facilityid;
+    const facility = await Facilities.findOne(facilityid);
+    if(!facility){
+        return res.status(404).json({message:"facility not found"})
+    }
+    return res.status(200).json(facility);
+}
+
+
--- backend/src/routes/adminRoutes.js
+++ backend/src/routes/adminRoutes.js
@@ -1,7 +1,8 @@
 import express from "express";
+import { approveFacility, getAllFacilities, getFacility } from "../controller/adminController";
 
 const router = express.Router({ mergeParams: true });
 
 router.get("/",getAllFacilities);
-router.get("/:facilityid");
-router.post("/:facilityid/approve");
+router.get("/:facilityid",getFacility);
+router.post("/:facilityid/approve",approveFacility);
